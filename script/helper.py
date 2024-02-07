2453d7351929ca1d7d4d0843cedf9b4f4b7c96c0
HPW-dev <hpwdev0@gmail.com>
2024-02-08 00:16:49 +0300

    правильное получение текущей версии

--- script/helper.py
+++ script/helper.py
@@ -33,7 +33,7 @@ def exec_cmd (cmd):
 
 def write_game_version():
   with open(file='src/game/util/version.cpp', mode='w', newline='\n') as file:
-    os.system('git describe --tags --abbrev=0 > _tmp_version_')
+    os.system('git tag --sort=committerdate | tail -1 > _tmp_version_')
     version = open(file='_tmp_version_', mode='r').read()
     version = version.replace('\r\n', '')
     version = version.replace('\n', '')
