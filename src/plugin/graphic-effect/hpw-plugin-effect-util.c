c876bde15d7b724303f8750b601fadff2d444951
HPW-dev <hpwdev0@gmail.com>
2024-02-12 22:24:31 +0300

    Plugin-effect: функция-хэлпер для теста параметров в плагине

--- src/plugin/graphic-effect/hpw-plugin-effect-util.c
+++ src/plugin/graphic-effect/hpw-plugin-effect-util.c
@@ -33,3 +33,21 @@ Pal8 rgb24_to_pal8(const struct Rgb24 src) {
   const double gray = src.r * 0.299 + src.g * 0.587 + src.b * 0.114;
   return round(gray * GRAY_MUL);
 }
+
+void check_params(const struct Context* context, struct Result* result) {
+  result->error = "";
+  result->version = DEFAULT_EFFECT_API_VERSION;
+  result->init_succsess = true;
+
+  #define iferror(cond, msg) if (cond) { \
+    result->error = "effect plugin: " msg; \
+    result->init_succsess = false; \
+    return; \
+  }
+  iferror( !context->dst, "context.dst is null");
+  iferror(context->h == 0, "context.h is 0");
+  iferror(context->w == 0, "context.w is 0");
+  iferror( !context->registrate_param_f32, "registrate_param_f32 is null");
+  iferror( !context->registrate_param_i32, "registrate_param_i32 is null");
+  #undef iferror
+}
--- src/plugin/graphic-effect/hpw-plugin-effect-util.h
+++ src/plugin/graphic-effect/hpw-plugin-effect-util.h
@@ -3,3 +3,4 @@
 
 struct Rgb24 pal8_to_rgb24(const Pal8 src);
 Pal8 rgb24_to_pal8(const struct Rgb24 src);
+void check_params(const struct Context* context, struct Result* result);
