32c53a54c83ad7d1f132b098155096ba72c18df1
HPW-dev <hpwdev0@gmail.com>
2024-02-16 23:08:26 +0300

    разделил скрипты сборки игры и плагинов

--- script/build-game-debug.py
+++ script/build-game-debug.py
@@ -2,8 +2,10 @@
 import helper
 
 script = "src/game/SConscript"
+plugin_script = "src/plugin/graphic-effect/cxx/SConscript"
 is_debug = 1
 helper.write_game_version()
 helper.exec_cmd(f'scons -j4 -Q debug={is_debug} -Q script={script}')
+helper.exec_cmd(f'scons -j4 -Q debug={is_debug} -Q script={plugin_script}')
 helper.rem_all("build/plugin/effect/*.a") # удалить ненужные .a файлы
 helper.exec_cmd('build/HPW')
