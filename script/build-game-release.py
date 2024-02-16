36a7b597b5174c75ab2ab12809b29ee156591edc
HPW-dev <hpwdev0@gmail.com>
2024-02-16 21:11:03 +0300

    теперь игра сама билдит плагины графики

--- script/build-game-release.py
+++ script/build-game-release.py
@@ -5,4 +5,5 @@ script = "src/game/SConscript"
 is_debug = 0
 helper.write_game_version()
 helper.exec_cmd(f'scons -j4 -Q debug={is_debug} -Q script={script}')
+helper.rem_all("build/plugin/effect/*.a") # удалить ненужные .a файлы
 helper.exec_cmd('build/HPW')
---
+++ src/plugin/graphic-effect/cxx/brightness.cpp
@@ -0,0 +1,11 @@
+#include "plugin/graphic-effect/hpw-plugin-effect.h"
+
+extern "C" void plugin_init(const struct context_t* context, struct result_t* result) {
+
+}
+
+extern "C" void plugin_apply(uint32_t state) {
+
+}
+
+extern "C" void plugin_finalize(void) {}
