f78a6b16312be551a5e9d4fb92e78b816b009db4
HPW-dev <hpwdev0@gmail.com>
2024-03-02 18:04:05 +0300

    + inline for safecall/saferet

--- src/util/safecall.hpp
+++ src/util/safecall.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 /// вызов функции с проверкой её валидности
-void safecall(auto func, auto ...args) {
+inline void safecall(auto func, auto ...args) {
   if (func)
     (*func)(args...);
 }
 
 /// вызов функции с проверкой её валидности и получение возврата
-auto saferet(auto func, auto ...args) {
+inline auto saferet(auto func, auto ...args) {
   if (func)
     return (*func)(args...);
   return {};
