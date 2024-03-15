987e9d11c3f9572d4158a8e15a07f95b8a58bd1c
HPW-dev <hpwdev0@gmail.com>
2024-03-15 19:59:17 +0300

    бесполезно в мейне ловить необработанные исключения

--- src/game/game-main.cpp
+++ src/game/game-main.cpp
@@ -1,20 +1,7 @@
-#include <iostream>
 #include "game-app.hpp"
-#include "util/error.hpp"
 
 int main(int argc, char *argv[]) {
-  try {
-    Game_app app(argc, argv);
-    app.run();
-  } catch (CN<std::exception> ex) {
-    std::cerr << "C++ error: " << ex.what() << std::endl;
-    return EXIT_FAILURE;
-  } catch (CN<hpw::Error> err) {
-    std::cerr << "HPW error: " << err.what() << std::endl;
-    return EXIT_FAILURE;
-  } catch (...) {
-    std::cerr << "Unknown error" << std::endl;
-    return EXIT_FAILURE;
-  }
+  Game_app app(argc, argv);
+  app.run();
   return EXIT_SUCCESS;
 }
