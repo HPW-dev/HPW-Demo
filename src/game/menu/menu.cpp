345014a49c0b9be99fd2a3908e809acc1800795a
HPW-dev <hpwdev0@gmail.com>
2024-03-31 19:11:06 +0300

    Menu: при зажатии клавиши, ускоряется выбор пунктов

--- src/game/menu/menu.cpp
+++ src/game/menu/menu.cpp
@@ -5,6 +5,45 @@
 #include "game/scene/scene-manager.hpp"
 #include "graphic/font/font.hpp"
 
+/// учитывает одиночное нажатие или зажатие клавиши
+bool check_pressed_or_holded (
+const hpw::keycode keycode,
+const uint UPDATES_THRESHOLD = 60,
+const uint FAST_UPDATES_THRESHOLD = 17
+) {
+  // сколько апдейтов надо подождать, чтоы зажатие сработало
+  static uint updates_threshold = UPDATES_THRESHOLD;
+  static auto last_pressed_keycode = hpw::keycode::error;
+  static uint hold_count = 0;
+  bool ret = false;
+
+  if (is_pressed_once(keycode)) {
+    ret = true;
+    last_pressed_keycode = keycode;
+    hold_count = 0;
+    // вернуть обратно число тиков, для применения автонажатия
+    updates_threshold = UPDATES_THRESHOLD;
+  } else {
+    if (is_pressed(keycode)) {
+      if (last_pressed_keycode == keycode)
+        ++hold_count;
+      else {
+        hold_count = 0;
+        // вернуть обратно число тиков, для применения автонажатия
+        updates_threshold = UPDATES_THRESHOLD;
+      }
+      last_pressed_keycode = keycode;
+    }
+  }
+  if (hold_count >= updates_threshold) {
+    hold_count = 0;
+    ret = true;
+    // ускорить автонажатие
+    updates_threshold = FAST_UPDATES_THRESHOLD;
+  }
+  return ret;
+} // check_pressed_or_holded
+
 Menu::Menu(CN<Menu_items> items)
 : m_items(items)
 { iflog(m_items.empty(), "m_items empty\n"); }
@@ -12,15 +51,15 @@ Menu::Menu(CN<Menu_items> items)
 void Menu::update(double dt) {
   return_if(m_items.empty());
 
-  if (is_pressed_once(hpw::keycode::enable))
+  if (check_pressed_or_holded(hpw::keycode::enable, 60, 60))
     m_items[m_cur_item]->enable();
-  if (is_pressed_once(hpw::keycode::left))
+  if (check_pressed_or_holded(hpw::keycode::left, 50, 18))
     m_items[m_cur_item]->minus();
-  if (is_pressed_once(hpw::keycode::right))
+  if (check_pressed_or_holded(hpw::keycode::right, 50, 18))
     m_items[m_cur_item]->plus();
-  if (is_pressed_once(hpw::keycode::down))
+  if (check_pressed_or_holded(hpw::keycode::down, 60, 30))
     next_item();
-  if (is_pressed_once(hpw::keycode::up))
+  if (check_pressed_or_holded(hpw::keycode::up, 60, 30))
     prev_item();
 } // update
 
