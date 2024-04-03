f7357a95894a288a8f88bd88aa352736c5ab16de
HPW-dev <hpwdev0@gmail.com>
2024-04-04 01:47:07 +0300

    PGE: brightness по умолчанию настроен на повышение яркости

--- src/plugin/graphic-effect/cxx/brightness.cpp
+++ src/plugin/graphic-effect/cxx/brightness.cpp
@@ -9,7 +9,7 @@
 NOT_EXPORTED Pal8* g_dst {}; // ссыль на растр от игры
 NOT_EXPORTED uint16_t g_w {}; // ширина растра
 NOT_EXPORTED uint16_t g_h {}; // высота растра
-NOT_EXPORTED int32_t g_value {}; // параметр яркости от эффекта
+NOT_EXPORTED int32_t g_value {32}; // параметр яркости от эффекта
 
 extern "C" EXPORTED void PLUG_CALL plugin_init(const struct context_t* context,
 struct result_t* result) {
