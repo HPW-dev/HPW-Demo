aa0251a11efde3d22c8cc4e1e0dc5f58f76e34d4
HPW-dev <hpwdev0@gmail.com>
2024-04-12 03:11:42 +0300

    PGE: + плагин подменяющий тайлы на похожие

--- src/plugin/graphic-effect/cxx/pge-util.cpp
+++ src/plugin/graphic-effect/cxx/pge-util.cpp
@@ -34,7 +34,7 @@ const int width, const Pal8 val)
   { image[x + width * y] = val; }
 
 NOT_EXPORTED
-Pal8 get_pixel_safe(Pal8 image[], const int x, const int y,
+Pal8 get_pixel_safe(const Pal8 image[], const int x, const int y,
 const int width, const int height) {
   if (x >= 0 && x < width && y >= 0 && y < height)
     return image[x + width * y];
--- src/plugin/graphic-effect/cxx/pge-util.hpp
+++ src/plugin/graphic-effect/cxx/pge-util.hpp
@@ -8,7 +8,7 @@ NOT_EXPORTED bool check_params(const context_t* context, result_t* result);
 // получить пиксель картинки быстро без проверок
 NOT_EXPORTED Pal8& get_pixel_fast(Pal8 image[], const int x, const int y, const int width);
 // получить пиксель картинки с проверками
-NOT_EXPORTED Pal8 get_pixel_safe(Pal8 image[], const int x, const int y,
+NOT_EXPORTED Pal8 get_pixel_safe(const Pal8 image[], const int x, const int y,
   const int width, const int height);
 // записать пиксель картинки быстро без проверок
 NOT_EXPORTED void set_pixel_fast(Pal8 image[], const int x, const int y, const int width,
