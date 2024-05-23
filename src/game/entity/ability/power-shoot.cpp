8983bc1db3b298bf77ed2fb5d84acee91ab09a66
HPW-Dev <hpwdev0@gmail.com>
2024-05-23 20:17:48 +0300

    Ability_power_shoot: запуск ведущих снарядов

--- src/game/entity/ability/power-shoot.cpp
+++ src/game/entity/ability/power-shoot.cpp
@@ -9,6 +9,7 @@
 #include "game/entity/util/phys.hpp"
 #include "game/entity/util/entity-util.hpp"
 #include "game/util/game-util.hpp"
+#include "game/util/keybits.hpp"
 #include "util/math/random.hpp"
 #include "util/hpw-util.hpp"
 #include "util/file/yaml.hpp"
@@ -24,11 +25,15 @@ struct Ability_power_shoot::Impl {
   hp_t m_energy_needed {}; // сколько должно быть энергии для мощного выстрела
   Str m_big_bullet {}; // ведущий снаряд
   Str m_small_bullet {}; // осколок ведущего снаряда
-  Minmax<uint> m_big_bullet_speed {};
-  Minmax<uint> m_small_bullet_speed {};
-  Minmax<uint> m_big_bullet_count {};
-  Minmax<uint> m_small_bullet_count {};
+  Minmax<real> m_big_bullet_speed {};
+  Minmax<real> m_small_bullet_speed {};
+  Minmax<int> m_big_bullet_count {};
+  Minmax<int> m_small_bullet_count {};
   real m_small_bullet_delay {};
+  real m_scatter_range {};
+  real m_scatter_power {};
+  real m_big_bullet_angle {}; // уго разброса ведущих пуль
+  real m_small_bullet_angle {}; // уго разброса ведущих пуль
 
   inline explicit Impl(CN<Player> player) {
     load_config(player);
@@ -36,49 +41,12 @@ struct Ability_power_shoot::Impl {
   }
 
   inline void update(Player& player, const double dt) {
-    /*return_if (player.energy < m_energy_for_power_shoot);
-
-    player.sub_en(m_power_shoot_price);
-    cfor (_, 30) { // TODO конфиг
-      cauto spawn_pos = player.phys.get_pos() + Vec(rndr(-7, 7), 0); // TODO конфиг
-      auto bullet = hpw::entity_mgr->make(&player, "bullet.player.mid", spawn_pos);
-      // пуля смотрит вверх в шмап моде
-      bullet->phys.set_deg(270);
-      // передача импульса для шмап мода
-      bullet->phys.set_speed( pps(rndr(13, 17)) ); // TODO конфиг
-      bullet->phys.set_vel(bullet->phys.get_vel() + player.phys.get_vel());
-      // разброс
-      bullet->phys.set_deg(bullet->phys.get_deg() + rndr(-75, 75)); // TODO конфиг
-      bullet->move_update_callback(get_spawner_small_bullets());
-      bullet->status.layer_up = true;
+    if (is_pressed(hpw::keycode::shoot) && player.energy >= m_energy_needed) {
+      player.sub_en(m_price);
+      make_bullets(player);
+      make_scatter(player);
     }
-
-    // отдача
-    hpw::entity_mgr->add_scatter(Scatter {
-      .pos{ player.phys.get_pos() + Vec(0, -10) }, // создаёт источник взрыва перед ноосом
-      .range{ 50 }, // TODO конфиг
-      .power{ pps(60) }, // TODO конфиг
-    });*/
   } // update
-
-  // спавнит мелкие пульки в след за мощным выстрелом
-  inline Entity::Update_callback get_spawner_small_bullets() const {
-    return [this](Entity& master, double dt)->void {
-      /*// TODO all vals by config
-      // TODO by dt timer
-      if ((hpw::game_updates_safe % 5) == 0) { // TODO game_updates_safe убрать
-        auto it = hpw::entity_mgr->make(&master, "bullet.player.small",
-          master.phys.get_pos());
-        // замедлить эти пули
-        it->phys.set_speed(it->phys.get_speed() * rndr(0.5, 1));
-        // чтобы пули разлетались во все стороны
-        it->phys.set_deg(it->phys.get_deg() + rndr(-45, 45));
-        it->phys.set_force( 7.5_pps );
-        it->move_update_callback( Kill_by_timeout(rndr(0.1, 0.7)) );
-        it->status.layer_up = false;
-      } // if timer*/
-    }; // ret lambda
-  } // spawn_small_bullets
 
   inline void powerup() {}
 
@@ -120,6 +88,7 @@ struct Ability_power_shoot::Impl {
       .min = big_bullet_count.at(0),
       .max = big_bullet_count.at(1)
     };
+    m_big_bullet_angle = big_bullet_node.get_real("angle");
 
     // опции осколков
     cauto small_bullet_node = root["small_bullet"];
@@ -136,6 +105,12 @@ struct Ability_power_shoot::Impl {
       .max = small_bullet_count.at(1)
     };
     m_small_bullet_delay = small_bullet_node.get_real("delay");
+    m_small_bullet_angle = small_bullet_node.get_real("angle");
+
+    // конфиг отдачи
+    cauto scatter_node = root["scatter"];
+    m_scatter_range = scatter_node.get_real("range");
+    m_scatter_power = scatter_node.get_real("power");
   } // load_config
 
   inline void test_config() const {
@@ -148,7 +123,56 @@ struct Ability_power_shoot::Impl {
     assert(m_big_bullet_count.max >= m_big_bullet_count.min);
     assert(m_small_bullet_count.max >= m_small_bullet_count.min);
     assert(m_small_bullet_delay > 0 && m_small_bullet_delay < 10);
+    assert(m_scatter_range > 0 && m_scatter_range < 100'000);
+    assert(m_scatter_power > 0 && m_scatter_power < 1'000'000);
   }
+
+  // запустить ведущие снаряды
+  inline void make_bullets(Player& player) {
+    cauto big_bullets = rnd(m_big_bullet_count.min, m_big_bullet_count.max);
+    cfor (_, big_bullets) {
+      cauto spawn_pos = player.phys.get_pos() + Vec(rndr(-7, 7), 0);
+      auto bullet = hpw::entity_mgr->make(&player, m_big_bullet, spawn_pos);
+      bullet->phys.set_deg(270); // пуля смотрит вверх в шмап моде
+      // передача импульса для шмап мода
+      cauto big_bullet_speed = rndr(m_big_bullet_speed.min, m_big_bullet_speed.max);
+      bullet->phys.set_speed(big_bullet_speed);
+      bullet->phys.set_vel(bullet->phys.get_vel() + player.phys.get_vel());
+      // разброс
+      cauto angle = rndr(-m_big_bullet_angle, m_big_bullet_angle);
+      bullet->phys.set_deg(bullet->phys.get_deg() + angle);
+      bullet->move_update_callback(get_spawner_small_bullets());
+      bullet->status.ignore_scatter = true;
+    }
+  }
+
+  // отдача от выстрела
+  inline void make_scatter(Player& player) {
+    hpw::entity_mgr->add_scatter(Scatter {
+      .pos{ player.phys.get_pos() + Vec(0, -10) }, // создаёт источник взрыва перед ноосом
+      .range {m_scatter_range},
+      .power {pps(m_scatter_power)},
+    });
+  }
+
+  // спавнит мелкие пульки в след за мощным выстрелом
+  inline Entity::Update_callback get_spawner_small_bullets() const {
+    return [this](Entity& master, double dt)->void {
+      /*// TODO all vals by config
+      // TODO by dt timer
+      if ((hpw::game_updates_safe % 5) == 0) { // TODO game_updates_safe убрать
+        auto it = hpw::entity_mgr->make(&master, "bullet.player.small",
+          master.phys.get_pos());
+        // замедлить эти пули
+        it->phys.set_speed(it->phys.get_speed() * rndr(0.5, 1));
+        // чтобы пули разлетались во все стороны
+        it->phys.set_deg(it->phys.get_deg() + rndr(-45, 45));
+        it->phys.set_force( 7.5_pps );
+        it->move_update_callback( Kill_by_timeout(rndr(0.1, 0.7)) );
+        it->status.layer_up = false;
+      } // if timer*/
+    }; // ret lambda
+  } // spawn_small_bullets
 }; // Impl
 
 Ability_power_shoot::Ability_power_shoot(CN<Player> player)
