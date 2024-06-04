568fd066e16914a6c3afb356cc3aed518867c006
HPW-Dev <hpwdev0@gmail.com>
2024-06-04 18:46:35 +0300

    Message_mgr: лишний инклуд

--- src/game/util/message.cpp
+++ src/game/util/message.cpp
@@ -4,7 +4,6 @@
 #include "message.hpp"
 #include "util/log.hpp"
 #include "game/core/fonts.hpp"
-#include "graphic/font/font.hpp"
 #include "graphic/image/image.hpp"
 #include "graphic/util/util-templ.hpp"
 #include "graphic/util/graphic-util.hpp"
