28c754672b833505e8792417accd5c8d0b5f7d56
HPW-Dev <hpwdev0@gmail.com>
2024-06-15 18:16:08 +0300

    Cmd: быстрое стирание команды при зажатом бэкспейсе

--- src/game/scene/scene-cmd.cpp
+++ src/game/scene/scene-cmd.cpp
@@ -17,7 +17,10 @@
 #include "game/util/game-util.hpp"
 
 struct Scene_cmd::Impl {
+  // сколько апдейтов надо зажимать удаление текста, чтобы он начал удаляться
+  constx auto TEXT_DELETE_TIMEOUT = 120;
   Image bg {};
+  uint m_text_delete_timer {};
 
   inline ~Impl() { hpw::text_input_mode = false; }
 
@@ -38,6 +41,16 @@ struct Scene_cmd::Impl {
     if (is_pressed_once(hpw::keycode::text_delete))
       if (!hpw::text_input.empty())
         hpw::text_input.resize(hpw::text_input.size() - 1);
+    // если зажали кнопку удаления текста
+    if (is_pressed(hpw::keycode::text_delete)) {
+      ++m_text_delete_timer;
+      if (m_text_delete_timer >= TEXT_DELETE_TIMEOUT) {
+        if (!hpw::text_input.empty() && ((m_text_delete_timer % 6) == 0))
+          hpw::text_input.resize(hpw::text_input.size() - 1);
+      }
+    } else {
+      m_text_delete_timer = 0;
+    }
     // выполнить команду
     if (is_pressed_once(hpw::keycode::enable)) {
       hpw::message_mgr->clear(); // убрать предыдущее сообщение
