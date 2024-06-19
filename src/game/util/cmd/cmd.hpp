e3270b7eb861f8549d1638ca0d0af5203f0d35aa
HPW-Dev <hpwdev0@gmail.com>
2024-06-19 13:04:18 +0300

    командная строка с пустой реализацией

--- src/game/util/cmd/cmd.hpp
+++ src/game/util/cmd/cmd.hpp
@@ -2,6 +2,21 @@
 #include "util/mem-types.hpp"
 #include "util/macro.hpp"
 #include "util/str.hpp"
+#include "util/unicode.hpp"
+#include "util/vector-types.hpp"
+
+// База для команд
+class Command {
+public:
+  Str name {}; // название команды
+  utf32 description {};
+  virtual ~Command() = default;
+  virtual void exec(CN<Strs> cmd_and_args);
+  // повлиять на автодополнение
+  inline virtual Strs add_matches(CN<Strs> cmd_and_args) const { return {}; }
+};
+
+using Commands = Vector<Unique<Command>>;
 
 // исполняет текстовые команды
 class Cmd {
@@ -12,13 +27,17 @@ public:
   Cmd();
   ~Cmd();
   // выполнить команду
-  void exec(CN<Str> command);
+  void exec(CN<Str> cmd_and_args);
   // получить прыдыдущую команду
   Str last_command() const;
   // найти совпадающие имена команд (для автодополнения)
-  Strs command_matches(CN<Str> command) const;
+  Strs command_matches(CN<Str> cmd_and_args) const;
+  // получить все загруженные команды
+  CN<Commands> commands() const;
+  // загрузить новую команду
+  void move(Unique<Command>&& command);
 }; // Cmd
 
 namespace hpw {
-inline Unique<Cmd> cmd {};
+inline Unique<Cmd> cmd {}; // исполняет команды в игре
 }
