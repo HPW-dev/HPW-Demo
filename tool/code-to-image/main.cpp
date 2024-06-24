304bf1e786ce79944a9b1922192115480aedf660
HPW-Dev <hpwdev0@gmail.com>
2024-06-24 17:59:15 +0300

    Code-to-image: поиск всех нужных файлов

--- tool/code-to-image/main.cpp
+++ tool/code-to-image/main.cpp
@@ -1,6 +1,78 @@
-#include "util/log.hpp"
+#include <filesystem>
+#include <iostream>
+#include <algorithm>
 #include "util/error.hpp"
+#include "util/str.hpp"
+#include "util/str-util.hpp"
+#include "util/platform.hpp"
+
+inline Str prepare_seach_dir(CN<Str> path) {
+  Str ret = path;
+  if (ret.empty())
+    ret = "./";
+  ret += '/';
+  conv_sep(ret);
+  return ret;
+}
+
+// true если имя файла подходит по маске
+inline bool check_file_masks(CN<Str> fname, CN<Strs> masks) {
+  for (cnauto mask: masks)
+    if (fname.find(mask) != Str::npos)
+      return true;
+  return false;
+}
+
+inline void recursive_find_helper(Strs& dst,
+CN<std::filesystem::directory_iterator> dir, CN<Strs> masks) {
+  for (cnauto file: dir) {
+    if (file.is_directory())
+      recursive_find_helper(
+        dst,
+        std::filesystem::directory_iterator(file.path().string() + SEPARATOR),
+        masks);
+
+    cauto fname = file.path().string();
+    if (check_file_masks(fname, masks))
+      dst.push_back(fname);
+  }
+}
+
+/* репурсивно ищет имена файлов подходящие по шаблону
+в masks по всем папкам в search_dir */
+Strs recursive_find(CN<Str> search_dir, CN<Strs> masks) {
+  Strs ret;
+  cauto dir = std::filesystem::directory_iterator(search_dir);
+  recursive_find_helper(ret, dir, masks);
+  return ret;
+}
+
+// убирает файлы соответствующие маске
+void exclude_mask(Strs& dst, CN<Strs> masks) {
+  std::erase_if(dst, [&](CN<Str> fname) {
+    for (cnauto mask: masks)
+      if (fname.find(mask) != Str::npos)
+        return true;
+    return false;
+  });
+}
 
 int main(const int argc, const char* argv[]) {
-  iferror(argc < 2, "Need more args.\nUsage: ./code-to-image __code_dir_name__");
+  // получить путь поиска файлов
+  iferror(argc < 2, "Need more args.\n"
+    "Usage: ./code-to-image __code_dir_name__");
+  cauto search_dir = prepare_seach_dir(argv[1]);
+  std::cout << "search dir: \"" << search_dir << '\"' << std::endl;
+
+  // получить список файлов
+  auto files = recursive_find(search_dir,
+    {".cpp", ".hpp", ".py", ".bat", ".sh", ".txt", ".yml", ".c",
+    ".h", ".cxx", ".hxx", ".tpp", "SConscript", "SCons", ".templ" });
+  
+  // убрать из списка неугодных
+  exclude_mask(files, {"thirdparty"});
+  iferror(files.empty(), "empty list of files in dir \"" + search_dir + '\"');
+  std::cout << "files:\n";
+  for (cnauto fname: files)
+    std::cout << "  \"" << fname << "\"\n";
 }
