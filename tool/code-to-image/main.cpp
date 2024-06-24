6565ed83092b12d58adb274c07cc6d2d85dc1f02
HPW-Dev <hpwdev0@gmail.com>
2024-06-24 17:19:58 +0300

    Code-to-image: проверка числа аргументов

--- tool/code-to-image/main.cpp
+++ tool/code-to-image/main.cpp
@@ -1,3 +1,6 @@
-int main() {
+#include "util/log.hpp"
+#include "util/error.hpp"
 
+int main(const int argc, const char* argv[]) {
+  iferror(argc < 2, "Need more args.\nUsage: ./code-to-image __code_dir_name__");
 }
