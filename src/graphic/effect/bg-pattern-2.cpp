1fe222450bae6dcf75e67b2afe6c04c16f770d90
HPW-Dev <hpwdev0@gmail.com>
2024-06-25 18:41:09 +0300

    заготовка под вторую пачку фонов

---
+++ src/graphic/effect/bg-pattern-2.cpp
@@ -0,0 +1,10 @@
+#include <omp.h>
+#include <cassert>
+#include "bg-pattern.hpp"
+#include "graphic/image/image.hpp"
+#include "graphic/util/graphic-util.hpp"
+#include "graphic/util/util-templ.hpp"
+#include "util/math/random.hpp"
+#include "game/core/fonts.hpp"
+
+
---
+++ src/graphic/effect/bg-pattern-2.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+class Image;
+
+// битовой узор 1
+//void bgp_bit_1(Image& dst, const int bg_state);
