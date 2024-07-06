9fa1460ddc21a29c523dda8fb512ab307f0a18ce
HPW-Dev <hpwdev0@gmail.com>
2024-07-06 18:44:26 +0300

    + comment

--- src/game/entity/collider/collider-simple.cpp
+++ src/game/entity/collider/collider-simple.cpp
@@ -34,7 +34,7 @@ void Collider_simple::test_collide(Entity& a, Entity& b) {
 
 void Collider_simple::operator()(CN<Entitys> entities, Delta_time dt) {
   cauto entitys_sz = entities.size();
-  return_if(entitys_sz <= 1);
+  return_if(entitys_sz <= 1); // защита от зацикливания
 
   // проверить столкновения пар объектов (без повторений)
   #pragma omp parallel for simd schedule(dynamic, 4)
