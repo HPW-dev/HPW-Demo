fa0bdbbee80a64acd1087a2f9a8a130fc642dac9
HPW-Dev <hpwdev0@gmail.com>
2024-07-18 06:18:53 +0300

    exit from entity-editor by ESC

--- tool/editor/scene/scene-entity-editor.cpp
+++ tool/editor/scene/scene-entity-editor.cpp
@@ -1,11 +1,23 @@
+#include <imgui.h>
 #include "scene-entity-editor.hpp"
+#include "yn.hpp"
 #include "graphic/image/image.hpp"
+#include "game/core/scenes.hpp"
 
 struct Scene_entity_editor::Impl {
   inline void update(const Delta_time dt) {}
   inline void draw(Image& dst) const {}
-  inline void imgui_exec() {}
-};
+
+  inline void imgui_exec() {
+    if (ImGui::IsKeyDown(ImGuiKey_Escape))
+      exit();
+  }
+
+  inline void exit() {
+    hpw::scene_mgr->add( new_shared<Yes_no_scene>(
+      u8"Выйти из редактора?", []{ hpw::scene_mgr->back(); } ) );
+  }
+}; // Impl
 
 Scene_entity_editor::Scene_entity_editor(): impl {new_unique<Impl>()} {}
 Scene_entity_editor::~Scene_entity_editor() {}
