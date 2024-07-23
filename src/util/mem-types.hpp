450bd7a625ee40d3168bad2de127f7910fc2e538
HPW-Dev <hpwdev0@gmail.com>
2024-07-23 19:07:46 +0300

    + macrosez init_unique/shared

--- src/util/mem-types.hpp
+++ src/util/mem-types.hpp
@@ -12,8 +12,18 @@ using Weak = std::__weak_ptr<T, __gnu_cxx::_S_single>;
 template <class T>
 using Unique = std::unique_ptr<T>;
 
+// выделяет данные в std::shared совместимом поинтере
 template <class T, typename... Args>
 Shared<T> new_shared(Args&&... args) { return Shared<T>(new T(args...)); }
 
+// выделяет данные в std::unique совместимом поинтере
 template <class T, typename... Args>
 Unique<T> new_unique(Args&&... args) { return Unique<T>(new T(args...)); }
+
+// инициализирует данные в std::unique совместимом поинтере
+template <typename... Args>
+void init_unique(auto& ptr, Args&&... args) { ptr = new_unique(args...); }
+
+// инициализирует данные в std::shared совместимом поинтере
+template <typename... Args>
+void init_shared(auto& ptr, Args&&... args) { ptr = new_shared(args...); }
