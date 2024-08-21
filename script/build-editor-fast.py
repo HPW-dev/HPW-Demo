378e79d95d5f1659b0d3e8267ec6b934404117a1
HPW-Dev <hpwdev0@gmail.com>
2024-08-21 21:14:19 +0300

    скрипт для облегчённого билда редактора (у меня 500мб весит проект в дебаге)

---
+++ script/build-editor-fast.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import helper
+
+script = "tool/editor/SConscript"
+is_debug = 1
+disable_debug_info = 1
+helper.exec_cmd(f'scons -j16 -Q debug={is_debug} -Q disable_debug_info={disable_debug_info} -Q script={script}')
+helper.exec_cmd('build/editor')
