f99a9397b77274bb10574f85a1cad596785b328f
HPW-Dev <hpwdev0@gmail.com>
2024-08-26 16:31:55 +0300

    фон bgp_liquid - градиентная красная жижа

--- src/graphic/effect/bg-pattern-3.cpp
+++ src/graphic/effect/bg-pattern-3.cpp
@@ -1,5 +1,6 @@
 #include <omp.h>
 #include <array>
+#include <cmath>
 #include <cassert>
 #include <algorithm>
 #include "bg-pattern-3.hpp"
@@ -15,5 +16,23 @@
 #include "util/math/xorshift.hpp"
 
 void bgp_liquid(Image& dst, const int bg_state) {
+  cauto state = bg_state * 2;
 
+  // сделать пятна
+  #pragma omp parallel for simd collapse(2)
+  cfor (y, dst.Y)
+  cfor (x, dst.X) {
+    real color =
+      std::sin((x - y - state) / 140.f) / 2.f -
+      std::cos((y +     state) / 100.f)     +
+      std::cos((x + y + state) /  80.f) / 2.f -
+      std::sin((y +     state) / 120.f);
+    color += 1.f;
+    color /= 2.f;
+    color = color / (1.f + std::abs(color));
+    dst(x, y) = Pal8::from_real(color);
+  }
+
+  fast_dither_bayer16x16_4bit(dst);
+  to_red(dst);
 }
