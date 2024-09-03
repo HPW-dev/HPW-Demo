e6e287767a1caace89fba211b7ebefd24e81b28c
HPW-Dev <hpwdev0@gmail.com>
2024-09-03 18:04:40 +0300

    неиспользуемые уровни вырезаны

--- src/game/level/level-1.cpp
+++
@@ -1,9 +0,0 @@
-#include "level-1.hpp"
-#include "game/core/fonts.hpp"
-#include "graphic/image/image.hpp"
-
-void Level_1::update(const Vec vel, Delta_time dt) {}
-
-void Level_1::draw(Image& dst) const {
-  graphic::font->draw(dst, {40, 70}, U"Заглушка уровень 1", &blend_past);
-}
