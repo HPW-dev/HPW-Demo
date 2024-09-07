88e79e5a0ad9f5fbe31ec12ae07369f626380faf
HPW-Dev <hpwdev0@gmail.com>
2024-09-07 22:49:59 +0300

    Обновление локализаций

--- src/game/core/locales.hpp
+++ src/game/core/locales.hpp
@@ -6,6 +6,6 @@ class Locale;
 
 namespace hpw {
 inline Shared<Store<Locale>> store_locale {}; // локализованные строки
-inline const Str fallback_font_path = "resource/locale/en.yml";
+inline const Str fallback_font_path = "resource/locale/EN (Google).yml";
 inline Str locale_path = fallback_font_path; // текущая выбранная локаль
 }
