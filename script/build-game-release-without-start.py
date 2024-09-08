544a00288d5df4727c8215037ce58c1b6df5d684
HPW-Dev <hpwdev0@gmail.com>
2024-09-08 22:09:09 +0300

    скрипты для компиляции без запуска

---
+++ script/build-game-release-without-start.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import helper
+
+script = "src/game/SConscript"
+plugin_script = "src/plugin/graphic-effect/cxx/SConscript"
+is_debug = 0
+helper.write_game_version()
+helper.exec_cmd(f'scons -j16 -Q debug={is_debug} -Q script={script}')
+helper.exec_cmd(f'scons -j16 -Q debug={is_debug} -Q script={plugin_script}')
+if not is_debug:
+  helper.rem_all("build/plugin/effect/*.a") # удалить ненужные .a файлы
