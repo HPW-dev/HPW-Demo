d119c9af402fde4e68465019e4db73c72ed6a31c
HPW-Dev <hpwdev0@gmail.com>
2024-09-12 04:31:04 +0300

    гладкий узор Перлена

--- src/graphic/effect/noise.cpp
+++ src/graphic/effect/noise.cpp
@@ -3,6 +3,23 @@
 #include <cmath>
 #include "noise.hpp"
 #include "graphic/image/image.hpp"
+#include "util/math/mat.hpp"
+
+/*
+constexpr real blerp(real c00, real c10, real c01, real c11, real tx, real ty) {
+  return std::lerp(std::lerp(c00, c10, tx), std::lerp(c01, c11, tx), ty);
+};
+*/
+  
+inline real coslerp(const real a, const real b, const real t) {
+  cauto f = (1.f - std::cos(t * PI)) * 0.5f;
+  return a * (1.f - f) + b * f;
+}
+
+inline real bcoslerp(const real c00, const real c10, const real c01, const real c11,
+const real tx, const real ty) {
+  return coslerp(coslerp(c00, c10, tx), coslerp(c01, c11, tx), ty);
+}
 
 void noise_2d(Image& dst, const Vec OFFSET, const int HARMONICS, const real STEP_START,
 const real STEP_MUL, const real AMPLIFY, const real SCALE) {
@@ -15,9 +32,6 @@ const real STEP_MUL, const real AMPLIFY, const real SCALE) {
 
   constexpr auto gen_noise = [](int x, int y)->real
     { return ((((x * 125'245) ^ (y * 142'124)) + 124'534) % 10'000) / 5'000.f - 1.f; };
-
-  constexpr auto blerp = [](real c00, real c10, real c01, real c11, real tx, real ty)->real
-    { return std::lerp(std::lerp(c00, c10, tx), std::lerp(c01, c11, tx), ty); };
 
   real step = STEP_START;
   Vector<real> heightmap(dst.size, 0);
@@ -37,7 +51,7 @@ const real STEP_MUL, const real AMPLIFY, const real SCALE) {
       cauto noise_10 = gen_noise(xi+1, yi+0) * AMPLIFY;
       cauto noise_01 = gen_noise(xi+0, yi+1) * AMPLIFY;
       cauto noise_11 = gen_noise(xi+1, yi+1) * AMPLIFY;
-      cauto height = blerp(noise_00, noise_10, noise_01, noise_11, xf - xi, yf - yi);
+      cauto height = bcoslerp(noise_00, noise_10, noise_01, noise_11, xf - xi, yf - yi);
       heightmap[y * dst.X + x] += height; 
     }
     step *= STEP_MUL;
