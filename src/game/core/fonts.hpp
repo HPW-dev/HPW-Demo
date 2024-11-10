5d5eb6c424548ebcb4834b1a346c546935c4dc90
HPW-Dev <hpwdev0@gmail.com>
2024-11-10 15:30:05 +0300

    + шрифт для магаза

--- src/game/core/fonts.hpp
+++ src/game/core/fonts.hpp
@@ -4,5 +4,6 @@
 
 namespace graphic {
 inline Unique<hpw::Font> font; // обычный шрифт игры
+inline Unique<hpw::Font> font_shop; // шрифт для надписей в магазине
 inline Unique<hpw::Font> system_mono; // моноширинный шрифт для системной информации
 }
--- src/game/util/game-util.cpp
+++ src/game/util/game-util.cpp
@@ -593,6 +593,7 @@ void load_fonts() {
   assert(hpw::archive);
   auto mem = hpw::archive->get_file("resource/font/unifont-13.0.06.ttf");
   init_unique<Unifont>(graphic::font, mem, 16, true);
+  init_unique<Unifont>(graphic::font_shop, mem, 32, true);
   init_unique<Unifont_mono>(graphic::system_mono, mem, 8, 16, true);
 }
 
