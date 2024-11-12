4970ec986259f4a2a6243f03e1cf8913731a507e
HPW-Dev <hpwdev0@gmail.com>
2024-11-12 22:44:34 +0300

    заготовка под новый варик лога

--- src/util/log.hpp
+++ src/util/log.hpp
@@ -1,35 +1,8 @@
 #pragma once
+#include <source_location>
+#include <string_view>
 
-#ifdef DEBUG
-#include <syncstream>
-#include <atomic>
-#include <mutex>
-#include <iostream>
-
-namespace debug_ns {
-  inline std::atomic_bool hpw_log_no_details {false};
-}
-
-#define hpw_log(MSG) { \
-  std::osyncstream synced_out(std::cout); \
-  if (debug_ns::hpw_log_no_details) \
-    synced_out << MSG; \
-  else \
-    synced_out << __FILE__ << ':' << __LINE__ << ": " << MSG; \
-}
-#define iflog(COND, MSG) { \
-  if (COND) \
-    hpw_log (MSG) \
-}
-#else
-#define hpw_log(MSG) {}
-#define iflog(COND, MSG) {}
-#endif
-
-#ifdef DETAILED_LOG
-#define detailed_log(MSG) hpw_log(MSG)
-#define detailed_iflog(COND, MSG) iflog(COND, MSG)
-#else
-#define detailed_log(MSG) {}
-#define detailed_iflog(COND, MSG) {}
-#endif
+void hpw_log(const std::string_view msg, const std::source_location location = std::source_location::current());
+void iflog(const bool cond, const std::string_view msg, const std::source_location location = std::source_location::current());
+void detailed_log(const std::string_view msg, const std::source_location location = std::source_location::current());
+void detailed_iflog(const bool cond, const std::string_view msg, const std::source_location location = std::source_location::current());
