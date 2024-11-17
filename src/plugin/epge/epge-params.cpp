a964450296086146dc5e26a48c39401668f34121
HPW-Dev <hpwdev0@gmail.com>
2024-11-17 21:46:37 +0300

    реализация параметров для EPGE

--- src/plugin/epge/epge-params.cpp
+++ src/plugin/epge/epge-params.cpp
@@ -1,5 +1,67 @@
+#include <cassert>
+#include <algorithm>
 #include "epge-params.hpp"
+#include "util/str-util.hpp"
 
 namespace epge {
+
+Param_int::Param_int(int& value): _value {value} {}
+void Param_int::set_value(cr<Str> val) { _value = s2n<int>(val); }
+void Param_int::set_max(cr<Str> val) { _max = s2n<int>(val); assert(_max > _min); }
+void Param_int::set_min(cr<Str> val) { _min = s2n<int>(val); assert(_max > _min); }
+void Param_int::set_step(cr<Str> step) { _step = s2n<int>(step); assert(_step > 0); }
+void Param_int::set_fast_step(cr<Str> step) { _fast_step = s2n<int>(step); assert(_fast_step > 0); }
+Str Param_int::get_value() const { return n2s(_value); }
+Str Param_int::get_max() const { return n2s(_max); }
+Str Param_int::get_min() const { return n2s(_min); }
+Str Param_int::get_step() const { return n2s(_step); }
+Str Param_int::get_fast_step() const { return n2s(_fast_step); }
+void Param_int::plus_value() { _value = std::clamp(_value + _step, _min, _max); }
+void Param_int::minus_value() { _value = std::clamp(_value - _step, _min, _max); }
+void Param_int::plus_value_fast() { _value = std::clamp(_value + _fast_step, _min, _max); }
+void Param_int::minus_value_fast() { _value = std::clamp(_value - _fast_step, _min, _max); }
+
+Param_double::Param_double(double& value): _value {value} {}
+void Param_double::set_value(cr<Str> val) { _value = s2n<double>(val); }
+void Param_double::set_max(cr<Str> val) { _max = s2n<double>(val); assert(_max > _min); }
+void Param_double::set_min(cr<Str> val) { _min = s2n<double>(val); assert(_max > _min); }
+void Param_double::set_step(cr<Str> step) { _step = s2n<double>(step); assert(_step > 0); }
+void Param_double::set_fast_step(cr<Str> step) { _fast_step = s2n<double>(step); assert(_fast_step > 0); }
+Str Param_double::get_value() const { return n2s(_value); }
+Str Param_double::get_max() const { return n2s(_max); }
+Str Param_double::get_min() const { return n2s(_min); }
+Str Param_double::get_step() const { return n2s(_step); }
+Str Param_double::get_fast_step() const { return n2s(_fast_step); }
+void Param_double::plus_value() { _value = std::clamp(_value + _step, _min, _max); }
+void Param_double::minus_value() { _value = std::clamp(_value - _step, _min, _max); }
+void Param_double::plus_value_fast() { _value = std::clamp(_value + _fast_step, _min, _max); }
+void Param_double::minus_value_fast() { _value = std::clamp(_value - _fast_step, _min, _max); }
+
+inline static bool s2bool(cr<Str> str) { 
+  cauto str_lower = str_tolower(str);
+  return_if (str_lower == "true", true);
+  return_if (str_lower == "+", true);
+  return_if (str_lower == "yes", true);
+  return_if (str_lower == "1", true);
+  return_if (str_lower == "on", true);
+  return_if (str_lower == "enable", true);
+  return_if (str_lower == "enabled", true);
+  return_if (str_lower == "ok", true);
+  return false;
+}
+
+inline static Str bool2s(const bool val) {
+  return val ? "true" : "false";
+}
+
+Param_bool::Param_bool(bool& value): _value {value} {}
+void Param_bool::set_value(cr<Str> val) { _value = s2bool(val); }
+Str Param_bool::get_value() const { return bool2s(_value); }
+Str Param_bool::get_max() const { return "true"; }
+Str Param_bool::get_min() const { return "false"; }
+void Param_bool::plus_value() { _value = true; }
+void Param_bool::minus_value() { _value = false; }
+void Param_bool::plus_value_fast() { _value = true; }
+void Param_bool::minus_value_fast() { _value = false; }
 
 } // epge ns 
--- src/plugin/epge/epge-params.hpp
+++ src/plugin/epge/epge-params.hpp
@@ -7,27 +7,92 @@
 namespace epge {
 
 // описание параметра для настроек плагина
-struct Param {
+class Param {
+public:
   Str title {}; // отображаемое название опции
   Str desc {}; // коммент с пояснением параметра
 
   virtual void set_value(cr<Str> val) = 0;
   virtual Str get_value() const = 0;
-  virtual inline void set_max(cr<Str> val) noexcept {} // максимальный предел параметра
-  virtual inline void set_min(cr<Str> val) noexcept {} // минимальный предел параметра
-  virtual inline Str get_max() const noexcept { return {}; }
-  virtual inline Str get_min() const noexcept { return {}; }
+  virtual inline void set_max(cr<Str> val) {} // максимальный предел параметра
+  virtual inline void set_min(cr<Str> val) {} // минимальный предел параметра
+  virtual inline Str get_max() const { return {}; }
+  virtual inline Str get_min() const { return {}; }
   virtual inline void set_step(cr<Str> step) {} // шаг с которым меняется параметр
-  virtual inline Str get_step() const noexcept { return {}; }
+  virtual inline Str get_step() const { return {}; }
   virtual inline void set_fast_step(cr<Str> step) {} // ускоренное изменение параметра
-  virtual inline Str get_fast_step() const noexcept { return {}; }
+  virtual inline Str get_fast_step() const { return {}; }
   virtual void plus_value() = 0;
   virtual void minus_value() = 0;
-  virtual inline void plus_value_fast() noexcept {}
-  virtual inline void minus_value_fast() noexcept {}
+  virtual inline void plus_value_fast() {}
+  virtual inline void minus_value_fast() {}
   virtual inline void enable() { plus_value(); }
 }; // Param
 
 using Params = Vector< Shared<Param> >;
+
+class Param_double final: public Param {
+  double& _value;
+  double _min {-999'999};
+  double _max {+999'999};
+  double _step {0.01};
+  double _fast_step {0.1};
+
+public:
+  explicit Param_double(double& value);
+  void set_value(cr<Str> val) final;
+  void set_max(cr<Str> val) final;
+  void set_min(cr<Str> val) final;
+  void set_step(cr<Str> step) final;
+  void set_fast_step(cr<Str> step) final;
+  Str get_value() const final;
+  Str get_max() const final;
+  Str get_min() const final;
+  Str get_step() const final;
+  Str get_fast_step() const final;
+  void plus_value() final;
+  void minus_value() final;
+  void plus_value_fast() final;
+  void minus_value_fast() final;
+}; // Param_double
+
+class Param_int final: public Param {
+  int& _value;
+  int _min {-999'999};
+  int _max {+999'999};
+  int _step {1};
+  int _fast_step {4};
+
+public:
+  explicit Param_int(int& value);
+  void set_value(cr<Str> val) final;
+  void set_max(cr<Str> val) final;
+  void set_min(cr<Str> val) final;
+  void set_step(cr<Str> step) final;
+  void set_fast_step(cr<Str> step) final;
+  Str get_value() const final;
+  Str get_max() const final;
+  Str get_min() const final;
+  Str get_step() const final;
+  Str get_fast_step() const final;
+  void plus_value() final;
+  void minus_value() final;
+  void plus_value_fast() final;
+  void minus_value_fast() final;
+}; // Param_int
+
+class Param_bool final: public Param {
+  bool& _value;
+public:
+  explicit Param_bool(bool& value);
+  void set_value(cr<Str> val) final;
+  Str get_value() const final;
+  Str get_max() const final;
+  Str get_min() const final;
+  void plus_value() final;
+  void minus_value() final;
+  void plus_value_fast() final;
+  void minus_value_fast() final;
+}; // Param_bool
 
 } // epge ns
