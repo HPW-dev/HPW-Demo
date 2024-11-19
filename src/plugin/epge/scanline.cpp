f8972a6fb67b639f3b3dc5dad6f2a0cf4c04b543
HPW-Dev <hpwdev0@gmail.com>
2024-11-19 23:37:46 +0300

    режим повышения яркости в epge::Scanlin

--- src/plugin/epge/scanline.cpp
+++ src/plugin/epge/scanline.cpp
@@ -6,14 +6,16 @@
 namespace epge {
 
 struct Scanline::Impl final {
-  double _shadow {0.25}; // степень затенения полосками
+  double _intense {0.25}; // степень затенения полосками
   int _type {0}; // тип полосок: 0 - горизонтальные, 1 - верт-е, 2 - сетка
+  bool _shading {true}; // true - уменьшать яркость в полосках. Иначе - увеличивать
 
   inline Str name() const noexcept { return "scanline"; }
   inline Str desc() const noexcept { return "CRT display scanline simulation"; }
 
   inline void draw(Image& dst) const noexcept {
-    cauto SCANLINE_SHADOW = Pal8::from_real(_shadow);
+    cauto SCANLINE_SHADOW = Pal8::from_real(_intense);
+    cauto BLEND_F = _shading ? blend_sub_safe : blend_add_safe;
 
     switch (_type) {
       default:
@@ -22,25 +24,25 @@ struct Scanline::Impl final {
         cfor (y, dst.Y)
           if (y % 2)
             cfor (x, dst.X)
-              dst(x, y) = blend_sub_safe(SCANLINE_SHADOW, dst(x, y));
+              dst(x, y) = BLEND_F(SCANLINE_SHADOW, dst(x, y), {});
         break;
       }
 
       case 1: { // vline
-        #pragma omp parallel for simd if (dst.size > 64 * 64)
+        #pragma omp parallel for simd collapse(2) if (dst.size > 64 * 64)
         cfor (y, dst.Y)
         cfor (x, dst.X)
           if (x % 2)
-            dst(x, y) = blend_sub_safe(SCANLINE_SHADOW, dst(x, y));
+            dst(x, y) = BLEND_F(SCANLINE_SHADOW, dst(x, y), {});
         break;
       }
-      
+
       case 2: { // grid
-        #pragma omp parallel for simd if (dst.size > 64 * 64)
+        #pragma omp parallel for simd collapse(2) if (dst.size > 64 * 64)
         cfor (y, dst.Y)
         cfor (x, dst.X)
           if ((x % 2) || (y % 2))
-            dst(x, y) = blend_sub_safe(SCANLINE_SHADOW, dst(x, y));
+            dst(x, y) = BLEND_F(SCANLINE_SHADOW, dst(x, y), {});
         break;
       }
     } // switch _type
@@ -48,8 +50,9 @@ struct Scanline::Impl final {
 
   inline epge::Params params() noexcept {
     return epge::Params {
-      new_shared<epge::Param_double>("shadow", "degree of shading by lines", _shadow, 0, 1, 0.01, 0.1),
-      new_shared<epge::Param_int>("type", "type of scanlines:\n0 - horizontal, 1 - vertical, 2 - grid",
+      new_shared<epge::Param_double>("intense", "degree of shading/lighting by lines", _intense, 0, 1, 0.01, 0.1),
+      new_shared<epge::Param_bool>("shading", "reduce brightness in lines", _shading),
+      new_shared<epge::Param_int>("line type", "type of scanlines:\n0 - horizontal, 1 - vertical, 2 - grid",
         _type, 0, 2, 1, 1),
     };
   }
