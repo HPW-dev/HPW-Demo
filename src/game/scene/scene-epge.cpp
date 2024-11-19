d86f6c53c1ebdaa5922bfe8b14bdd828695880f1
HPW-Dev <hpwdev0@gmail.com>
2024-11-19 22:38:31 +0300

    сброс в epge так же сбрасывает обычный pge

--- src/game/scene/scene-epge.cpp
+++ src/game/scene/scene-epge.cpp
@@ -9,9 +9,10 @@
 #include "game/util/keybits.hpp"
 #include "game/util/locale.hpp"
 #include "game/util/game-util.hpp"
-#include "plugin/epge/epge-util.hpp"
+#include "game/util/pge.hpp"
 #include "game/menu/advanced-text-menu.hpp"
 #include "game/menu/item/text-item.hpp"
+#include "plugin/epge/epge-util.hpp"
 
 struct Scene_epge::Impl {
   Unique<Advanced_text_menu> _menu {};
@@ -75,6 +76,7 @@ struct Scene_epge::Impl {
     // ресет и выход
     menu_items.push_back( new_shared<Menu_text_item>( get_locale_str("common.reset"), [this]{
       hpw::epges.clear();
+      disable_pge();
       _need_reinit_menu = true;
     } ) );
     menu_items.push_back( new_shared<Menu_text_item>( get_locale_str("common.exit"), []{ exit_from_scene(); } ) );
