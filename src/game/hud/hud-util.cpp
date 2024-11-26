9a75be4771838736171220787afe1199af1b0519
HPW-Dev <hpwdev0@gmail.com>
2024-11-27 02:39:22 +0300

    сортировка имён интерфейсов

--- src/game/hud/hud-util.cpp
+++ src/game/hud/hud-util.cpp
@@ -14,18 +14,20 @@ namespace {
 using Hud_maker = std::function< Unique<Hud> () >;
 
 sconst std::unordered_map<Str, Hud_maker> g_huds {
-  {"none", []{ return new_unique<Hud_none>(); }},
-  {"roman", []{ return new_unique<Hud_roman>(); }},
-  {"hex", []{ return new_unique<Hud_hex>(); }},
-  {"minimal", []{ error("need impl for minimal HUD"); return Unique<Hud>{}; /*return new_unique<Hud_minimal>();*/ }},
-  {"asci", []{ return new_unique<Hud_asci>(); }},
+  {"NONE", []{ return new_unique<Hud_none>(); }},
+  {"BRAILLE", []{ error("need impl for Braille HUD"); return Unique<Hud>{}; }}, // TODO
+  {"HEX", []{ return new_unique<Hud_hex>(); }},
+  {"ROMAN II", []{ error("need impl for Roman II HUD"); return Unique<Hud>{}; }}, // TODO
+  {"ROMAN I", []{ return new_unique<Hud_roman>(); }},
+  {"MINIMAL", []{ error("need impl for Minimal HUD"); return Unique<Hud>{}; /*return new_unique<Hud_minimal>();*/ }}, // TODO
+  {"ASCI", []{ return new_unique<Hud_asci>(); }},
 };
 
 } // empty ns
 
 Unique<Hud> make_hud(cr<Str> name) {
   try {
-    return ::g_huds.at(str_tolower(name)) ();
+    return ::g_huds.at(str_toupper(name)) ();
   } catch (...) {
     hpw_log("не удалось создать интерфейс с именем \"" + name +
       "\". Будет создан Hud_asci по умолчанию\n", Log_stream::warning);
@@ -38,5 +40,6 @@ Strs hud_names() {
   Strs names;
   for (crauto [name, _]: ::g_huds)
     names.push_back(name);
+  std::sort(names.begin(), names.end());
   return names;
 }
