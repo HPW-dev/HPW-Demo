44d7ef08bb64b34150965733a0b04c43ebd28033
HPW-Dev <hpwdev0@gmail.com>
2024-12-10 06:30:46 +0300

    опции для управление Shuffler

--- src/plugin/epge/shuffler.cpp
+++ src/plugin/epge/shuffler.cpp
@@ -5,6 +5,13 @@
 namespace epge {
 
 struct Shuffler::Impl final {
+  int _seed {97'997}; // ALMONDS
+  int _block_sz {8};
+  bool _rotate_blocks {true};
+  bool _shuffle_blocks {true};
+  bool _randomize_blocks {false};
+  double _randomize_speed {1.0};
+
   inline Str name() const noexcept { return "shuffler"; }
   inline Str desc() const noexcept { return "breaks image into squares and shuffles them"; }
 
@@ -14,7 +21,14 @@ struct Shuffler::Impl final {
   }
 
   inline epge::Params params() noexcept {
-    return epge::Params {}; // TODO
+    return epge::Params {
+      new_shared<epge::Param_int>("seed", "value of the random generator that shuffles image blocks", _seed, 0, 0x7FFF'FFFF, 1, 10),
+      new_shared<epge::Param_int>("block size", "size of cut image blocks", _block_sz, 1, 512, 1, 4),
+      new_shared<epge::Param_bool>("blocks rotation", "apply image block rotation", _rotate_blocks),
+      new_shared<epge::Param_bool>("blocks shuffling", "apply image block shuffling", _shuffle_blocks),
+      new_shared<epge::Param_bool>("randomize blocks", "apply randomization of image blocks", _randomize_blocks),
+      new_shared<epge::Param_double>("randomization speed", "speed of applying image block randomization", _randomize_speed, 0, 100, 0.01, 0.05),
+    };
   }
 }; // Impl
 
