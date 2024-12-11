90eef7b9d43b97b22c0d0d509c8638c22f0c12d5
HPW-Dev <hpwdev0@gmail.com>
2024-12-12 02:27:44 +0300

    готов эффект Pixels_per_frame

--- src/plugin/epge/pixels-per-frame.cpp
+++ src/plugin/epge/pixels-per-frame.cpp
@@ -7,19 +7,34 @@
 namespace epge {
 
 struct Pixels_per_frame::Impl final {
-  int _ppf {800};
+  mutable Image _buffer {};
+  mutable int _cur_pos {}; // позиция текущего рисуемого пикселя
+  int _ppf {512 * 211};
 
   inline Str name() const noexcept { return "pixels per frame"; }
   inline Str desc() const noexcept { return "draws a certain number of pixels per frame"; }
 
   inline void draw(Image& dst) const noexcept {
     assert(dst);
-    // TOD
+    assert(_ppf > 0);
+
+    _buffer = dst;
+    dst.fill(Pal8::black);
+    cauto dst_size = dst.size;
+    
+    #pragma omp parallel for simd
+    cfor (i, _ppf) {
+      cauto idx = (_cur_pos + i) % dst_size;
+      dst[idx] = _buffer[idx];
+    }
+
+    _cur_pos += _ppf;
+    _cur_pos %= dst_size;
   }
 
   inline epge::Params params() noexcept {
     return epge::Params {
-      new_shared<epge::Param_int>("pixels per frame", "number of pixels per frame", _ppf, 1, 512*384 - 1, 4, 40),
+      new_shared<epge::Param_int>("pixels per frame", "number of pixels per frame", _ppf, 1, 512*384 - 1, 100, 1000),
     };
   }
 }; // Impl
