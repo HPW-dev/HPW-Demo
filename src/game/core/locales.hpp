faf88e625cc47d3bd7c6336387413c8767f74428
HPW-Dev <hpwdev0@gmail.com>
2024-12-22 00:12:43 +0300

    Русская локализация выбрана для отката

--- src/game/core/locales.hpp
+++ src/game/core/locales.hpp
@@ -6,6 +6,6 @@ class Locale;
 
 namespace hpw {
 inline Shared<Store<Locale>> store_locale {}; // локализованные строки
-inline const Str fallback_locale_path = "resource/locale/EN (Google).yml";
+inline const Str fallback_locale_path = "resource/locale/RU (Mramor).yml";
 inline Str locale_path = fallback_locale_path; // текущая выбранная локаль
 }
