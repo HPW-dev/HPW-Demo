1627a30aebb19cffc76d538d035fa337654c3e32
HPW-Dev <hpwdev0@gmail.com>
2024-12-29 09:07:58 +0300

    скрипты на билд релизного редактора

--- script/build-editor-release.py
+++ script/build-editor-release.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 import helper
 
-script = "tool/editor/SConscript"
-is_debug = 0
-helper.exec_cmd(f'scons -j16 -Q debug={is_debug} -Q script={script}')
+opts = \
+  ' -Q enable_omp=1' \
+  ' -Q enable_asan=0' \
+  ' -Q host=glfw3' \
+  ' -Q compiler=gcc' \
+  ' -Q use_data_zip=0' \
+  ' -Q opt_level=stable'
+NUM_THREADS = helper.get_max_threads() + 1
+print(f'threads for building: {NUM_THREADS}')
+helper.exec_cmd(f'scons -j{NUM_THREADS} -Q script=tool/editor/SConscript' + opts)
 helper.exec_cmd('build/bin/editor')
