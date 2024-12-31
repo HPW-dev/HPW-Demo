b10f355390446e1e717cd7d3f908bf043cf921fb
HPW-Dev <hpwdev0@gmail.com>
2024-12-31 07:07:01 +0300

    EPGE flashes готов

--- src/plugin/epge/flashes.cpp
+++ src/plugin/epge/flashes.cpp
@@ -1,10 +1,10 @@
-#include <omp.h>
 #include <cassert>
+#include <random>
+#include <ctime>
 #include "flashes.hpp"
 #include "graphic/image/image.hpp"
 #include "graphic/util/util-templ.hpp"
 #include "graphic/effect/light.hpp"
-#include "util/math/random.hpp"
 
 namespace epge {
 
@@ -15,16 +15,38 @@ struct Flashes::Impl final {
     MAX,
   };
 
-  int _mode {scast<int>(Mode::full_star)};
-  int _radius {7};
-  double _chance {0.5};
+  int _mode {scast<int>(Mode::diagonal)};
+  int _radius {1};
+  double _chance {0.16};
+  double _threshold {0.75};
+  bool _halo {false};
+  mutable std::default_random_engine _generator {};
+  mutable std::uniform_real_distribution<double> _dist {0, 1};
 
   inline Str name() const noexcept { return "flashes"; }
   inline Str desc() const noexcept { return "draws flashes on bright pixels"; }
 
+  inline Impl() { _generator.seed( std::time({}) ); }
+  
   inline void draw(Image& dst) const noexcept {
     assert(dst);
-    // #pragma omp parallel for simd collapse(2) if (dst.size > 64 * 64)
+    cauto is_diagonal = scast<Mode>(_mode) == Mode::diagonal ? true : false;
+
+    cfor (y, dst.Y)
+    cfor (x, dst.X) {
+      cauto ratio = dst(x, y).to_real();
+      continue_if (ratio < _threshold);
+      continue_if (_dist(_generator) > _chance);
+      Light lgt;
+      lgt.radius = _radius * ratio;
+      lgt.flags.random_radius = true;
+      lgt.flags.repeat = true;
+      lgt.flags.no_sphere = !_halo;
+      lgt.flags.star = !is_diagonal;
+      lgt.bf = &blend_max;
+      lgt.bf_star = &blend_max;
+      lgt.draw(dst, Vec(x, y));
+    }
   }
 
   inline epge::Params params() noexcept {
@@ -33,6 +55,8 @@ struct Flashes::Impl final {
         _mode, 0, scast<int>(Mode::MAX)-1, 1, 1),
       new_shared<epge::Param_int>("radius", "flash size", _radius, 1, 100, 1, 2),
       new_shared<epge::Param_double>("chance", "chance of a pixel flash", _chance, 0.01, 1, 0.01, 0.05),
+      new_shared<epge::Param_double>("threadid", "brightness threshold for flash to occur", _threshold, 0, 1, 0.05, 0.1),
+      new_shared<epge::Param_bool>("halo", "display flash halo", _halo),
     };
   }
 }; // Impl
