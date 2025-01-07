1ac0fcdb936715cfb9cb1829c024d77bb6bb5089
HPW-Dev <hpwdev0@gmail.com>
2025-01-07 05:55:22 +0300

    скрипт на запуск теста интернета

---
+++ script/net-test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import helper
+
+opts = \
+  ' -Q enable_asan=0' \
+  ' -Q compiler=gcc' \
+  ' -Q opt_level=debug'
+NUM_THREADS = helper.get_max_threads() + 1
+print(f'threads for building: {NUM_THREADS}')
+helper.exec_cmd(f'scons -j{NUM_THREADS} -Q script=test/net-test/SConscript' + opts)
+helper.exec_cmd('build/bin/net-test')
