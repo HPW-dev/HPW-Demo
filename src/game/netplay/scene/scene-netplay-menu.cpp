45ecafc1ac8f8011c1d41064f8a6be1aa1e6bfac
HPW-Dev <hpwdev0@gmail.com>
2025-01-11 07:42:57 +0300

    больше не нужно хранить функции отдельно

--- src/game/netplay/scene/scene-netplay-menu.cpp
+++ src/game/netplay/scene/scene-netplay-menu.cpp
@@ -1,7 +1,7 @@
 #include <cassert>
 #include "scene-netplay-menu.hpp"
 #include "game/menu/menu-from-yaml.hpp"
-#include "game/menu/item/text-item.hpp"
+#include "game/menu/item/item.hpp"
 #include "game/core/scenes.hpp"
 #include "game/util/resource-helper.hpp"
 #include "game/util/keybits.hpp"
@@ -11,25 +11,19 @@
 #include "graphic/image/image.hpp"
 
 struct Scene_netplay_menu::Impl {
-  Menu_text_item::Action _goto_find_server_scene {};
-  Menu_text_item::Action _goto_connect_by_ipv4_scene {};
-  Menu_text_item::Action _goto_create_server_scene {};
-  Action_table _actions {};
   Unique<Menu> _menu {};
 
   inline Impl() {
-    _goto_find_server_scene = []{ error("need impl"); };
-    _goto_connect_by_ipv4_scene = []{ error("need impl"); };
-    _goto_create_server_scene = []{ error("need impl"); };
-    _actions = Action_table {
-      {"goto_find_server_scene", Action_container(_goto_find_server_scene)},
-      {"goto_connect_by_ipv4_scene", Action_container(_goto_connect_by_ipv4_scene)},
-      {"goto_create_server_scene", Action_container(_goto_create_server_scene)},
-    };
-
     cauto config_file = load_res("resource/menu/netplay-menu.yml");
     Yaml config(config_file);
-    _menu = menu_from_yaml(config, _actions);
+    _menu = menu_from_yaml(
+      config,
+      Action_table {
+        {"goto_find_server_scene", Action_container( Menu_item::Action([]{ error("need impl"); }) )},
+        {"goto_connect_by_ipv4_scene", Action_container( Menu_item::Action([]{ error("need impl"); }) )},
+        {"goto_create_server_scene", Action_container( Menu_item::Action([]{ error("need impl"); }) )},
+      }
+    );
   }
 
   inline void update(const Delta_time dt) {
