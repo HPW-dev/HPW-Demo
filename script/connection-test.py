2c6cdb617df8f34c85b23532f2166226fa6042af
HPW-Dev <hpwdev0@gmail.com>
2025-01-22 02:03:29 +0300

    заготовки под два новых теста сети

---
+++ script/connection-test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import helper
+
+opts = \
+  ' -Q use_netplay=1' \
+  ' -Q opt_level=debug'
+NUM_THREADS = helper.get_max_threads() + 1
+print(f'threads for building: {NUM_THREADS}')
+helper.exec_cmd(f'scons -j{NUM_THREADS} -Q script=test/connection-test/SConscript' + opts)
+helper.exec_cmd('build/bin/connection-test')
