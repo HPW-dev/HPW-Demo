5974a15c1266172b1b2a9a1aaab7fdaf93721ba5
HPW-Dev <hpwdev0@gmail.com>
2025-01-23 23:15:25 +0300

    обновление строк меню после выбора другой локализации

--- src/game/scene/scene-options.cpp
+++ src/game/scene/scene-options.cpp
@@ -1,57 +1,3 @@
-/*
-#include "scene-options.hpp"
-#include "scene-mgr.hpp"
-#include "scene-input.hpp"
-#include "scene-graphic.hpp"
-#include "scene-game-options.hpp"
-#include "scene-hud-select.hpp"
-#include "scene-locale.hpp"
-#include "game/core/fonts.hpp"
-#include "game/core/scenes.hpp"
-#include "game/util/keybits.hpp"
-#include "game/util/locale.hpp"
-#include "game/util/blur-helper.hpp"
-#include "game/menu/text-menu.hpp"
-#include "game/menu/item/bool-item.hpp"
-#include "game/menu/item/text-item.hpp"
-#include "graphic/image/image.hpp"
-
-Scene_options::Scene_options() {
-  init_menu();
-}
-
-void Scene_options::update(const Delta_time dt) {
-  if (is_pressed_once(hpw::keycode::escape))
-    hpw::scene_mgr.back();
-  menu->update(dt);
-
-  // чтобы перезагрузить локализацию строк
-  if (hpw::scene_mgr.status.came_back)
-    init_menu();
-}
-
-void Scene_options::draw(Image& dst) const {
-  dst.fill(Pal8::black);
-  menu->draw(dst);
-  graphic::font->draw(dst, {20, 30}, get_locale_str("options.title"), &blend_max);
-}
-
-void Scene_options::init_menu() {
-  init_shared<Text_menu>( menu,
-    Menu_items {
-      new_shared<Menu_text_item>(get_locale_str("options.graphic"), []{ hpw::scene_mgr.add(new_shared<Scene_graphic>()); }),
-      new_shared<Menu_text_item>(get_locale_str("hud_select.title"), []{ hpw::scene_mgr.add(new_shared<Scene_hud_select>()); }),
-      // TODO new_shared<Menu_text_item>(get_locale_str("options.sound"), []{ hpw_log("need impl. for Sound settings\n"); }),
-      new_shared<Menu_text_item>(get_locale_str("options.input"), []{ hpw::scene_mgr.add(new_shared<Scene_input>()); }),
-      new_shared<Menu_text_item>(get_locale_str("game_opts.title"), []{ hpw::scene_mgr.add(new_shared<Scene_game_options>()); }),
-      new_shared<Menu_text_item>(hpw::locale_select_title, []{ hpw::scene_mgr.add(new_shared<Scene_locale_select>()); }),
-      new_shared<Menu_text_item>(get_locale_str("common.exit"), []{ hpw::scene_mgr.back(); }),
-    },
-    Vec{60, 80}
-  );
-} // init_menu
-*/
-
 #include <cassert>
 #include "scene-options.hpp"
 #include "scene-input.hpp"
@@ -70,7 +16,28 @@ void Scene_options::init_menu() {
 struct Scene_options::Impl {
   Unique<Menu> _menu {};
 
-  inline Impl() {
+  inline Impl() { init_menu(); }
+
+  inline void update(const Delta_time dt) {
+    if (is_pressed_once(hpw::keycode::escape))
+      hpw::scene_mgr.back();
+
+    assert(_menu);
+    _menu->update(dt);
+
+    // чтобы перезагрузить локализацию строк
+  if (hpw::scene_mgr.status.came_back)
+    init_menu();
+  }
+
+  inline void draw(Image& dst) const {
+    dst.fill(Pal8::black);
+
+    assert(_menu);
+    _menu->draw(dst);
+  }
+
+  inline void init_menu() {
     cauto config_file = load_res("resource/menu/options.yml");
     Yaml config(config_file);
     _menu = menu_from_yaml(
@@ -82,21 +49,6 @@ struct Scene_options::Impl {
         {"game_opts", Action_container( Menu_item::Action([]{ hpw::scene_mgr.add(new_shared<Scene_game_options>()); }) )},
       }
     );
-  }
-
-  inline void update(const Delta_time dt) {
-    if (is_pressed_once(hpw::keycode::escape))
-      hpw::scene_mgr.back();
-
-    assert(_menu);
-    _menu->update(dt);
-  }
-
-  inline void draw(Image& dst) const {
-    dst.fill(Pal8::black);
-
-    assert(_menu);
-    _menu->draw(dst);
   }
 }; // Impl 
 
