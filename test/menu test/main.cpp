5421d335d9f86eadacd169d720561ac3431d6bf8
HPW-Dev <hpwdev0@gmail.com>
2025-01-24 09:19:13 +0300

    - лишние тесты

--- test/menu test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory>
-#include "host/glfw3/host-glfw.hpp"
-#include "menu-test.hpp"
-
-int main(int argc, char *argv[]) {
-  auto main_func = new_shared<Menu_test>();
-  Host_glfw host(argc, argv, main_func);
-  host.execute();
-}
