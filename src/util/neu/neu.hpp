54dd5cd79318426887ea155998788cb682cacdc6
Ilya Halilov <hpwdev0@gmail.com>
2025-02-10 14:06:41 +0300

    интерфейс neu::Base

--- src/util/neu/neu.hpp
+++ src/util/neu/neu.hpp
@@ -1,3 +1,30 @@
 #pragma once
+#include "util/vector-types.hpp"
+#include "util/math/num-types.hpp"
+#include "util/macro.hpp"
 
+class Yaml;
 
+namespace neu {
+  
+using Weight = real;
+using Weights = Vector<Weight>;
+
+class Base {
+public:
+  Base() = default;
+  virtual ~Base() = default;
+
+  virtual void save(Yaml& dst) = 0;
+  virtual void load(cr<Yaml> src) = 0;
+  virtual Base* operator =(cr<Base> other) = 0;
+  virtual void update() = 0;
+
+  inline Weights& hiden_weights() { return _hiden_weights; };
+  inline cr<Weights> hiden_weights() const { return _hiden_weights; };
+
+private:
+  Weights _hiden_weights {}; // все веса скрытых слоёв нейросети
+}; // Base
+
+} // neu ns
