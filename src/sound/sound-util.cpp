e4ec4a3e774a04af67316c89c9c91ec893d09acf
Ilya Halilov <hpwdev0@gmail.com>
2025-03-10 11:04:44 +0300

    функции утилиты

--- src/sound/sound-util.cpp
+++ src/sound/sound-util.cpp
@@ -1,20 +1,34 @@
+#include "sound.hpp"
 #include "sound-util.hpp"
 #include "util/log.hpp"
 
 namespace sound {
 
 void kill_all() {
-  log_warning << "TODO need impl"; // TODO
+  Track::Config cfg { .playing = false };
+  
+  log_debug << "kill all tracks";
+  auto tracks = all_tracks();
+  for (crauto tr: tracks)
+    tr->set_config(cfg);
 }
 
 void stop_all() {
+  Track::Config cfg { .stoped = true };
   
-  log_warning << "TODO need impl"; // TODO
+  log_debug << "stop all tracks";
+  auto tracks = all_tracks();
+  for (crauto tr: tracks)
+    tr->set_config(cfg);
 }
 
 void continue_all() {
+  Track::Config cfg { .stoped = false };
   
-  log_warning << "TODO need impl"; // TODO
+  log_debug << "continue all tracks";
+  auto tracks = all_tracks();
+  for (crauto tr: tracks)
+    tr->set_config(cfg);
 }
 
 } // sound ns
