c498bc6c43eaead40943b0beafb61f6faf0c546d
HPW-Dev <hpwdev0@gmail.com>
2025-09-04 06:12:52 +0300

    заготовка теста реакции

--- src/game/scene/scene-input-delay-test.cpp
+++ src/game/scene/scene-input-delay-test.cpp
@@ -1,21 +1,38 @@
 #include <cassert>
 #include "scene-input-delay-test.hpp"
 #include "game/core/scenes.hpp"
+#include "game/core/fonts.hpp"
+#include "graphic/font/font-util.hpp"
 #include "game/util/locale.hpp"
 #include "game/util/keybits.hpp"
 #include "graphic/util/util-templ.hpp"
 #include "graphic/util/graphic-util.hpp"
 
 struct Scene_input_delay_test::Impl {
-  inline explicit Impl() {}
+  utf32 info_text {};
+
+  inline explicit Impl() {
+     info_text += get_locale_str("input_delay_test.info_1");
+     info_text += U" <" + hpw::keys_info.find(hpw::keycode::shoot)->name + U"> ";
+     info_text += get_locale_str("input_delay_test.info_2") + U". ";
+     info_text += get_locale_str("input_delay_test.info_3");
+     info_text += U" <" + hpw::keys_info.find(hpw::keycode::bomb)->name + U">";
+  }
 
   inline void update(const Delta_time dt) {
     if (is_pressed_once(hpw::keycode::escape))
       hpw::scene_mgr.back();
+
+    if (is_pressed_once(hpw::keycode::bomb))
+      {}
+
+    if (is_pressed_once(hpw::keycode::shoot))
+      {}
   }
 
   inline void draw(Image& dst) const {
     dst.fill({});
+    text_bordered(dst, info_text, graphic::font.get(), {5, 150, dst.X, 400}, {5, 5});
   }
 }; // Impl
 
