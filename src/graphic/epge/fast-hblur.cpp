0b043a1beceaaf96d17aca8fabeb56c18820b1c3
HPW-Dev <hpwdev0@gmail.com>
2025-09-05 23:49:10 +0300

    код эффекта готов

--- src/graphic/epge/fast-hblur.cpp
+++ src/graphic/epge/fast-hblur.cpp
@@ -39,6 +39,32 @@ public:
       }
     }
   }
+  
+  void draw_bidir(Image& dst) const {
+    #pragma omp parallel for
+    for (int i = _window_sz; i < dst.size - _window_sz; ++i) {
+      for (int wnd = -_window_sz; wnd < _window_sz + 1; ++wnd) {
+        dst[i] = blend_avr(dst[i + wnd], dst[i]);
+        dst[i + wnd] = dst[i];
+      }
+    }
+  }  
+
+  void draw_accurate(Image& dst) const {
+    static Image buf;
+    buf = dst;
+    const real DIV = 1.0 / (_window_sz * 2 + 1);
+
+    #pragma omp parallel for
+    for (int i = _window_sz; i < dst.size - _window_sz; ++i) {
+      real total = 0;
+
+      for (int wnd = -_window_sz; wnd < _window_sz + 1; ++wnd)
+        total += buf[i + wnd].to_real();
+      
+      dst[i] = Pal8::from_real(total * DIV);
+    }
+  }
 
   void draw(Image& dst) const {
     assert(dst);
@@ -47,6 +73,8 @@ public:
       default:
       case 0: draw_blur(dst); break;
       case 1: draw_full_average(dst); break;
+      case 2: draw_bidir(dst); break;
+      case 3: draw_accurate(dst); break;
     }
   }
 
@@ -54,7 +82,7 @@ public:
     return Params {
       //new_shared<Param_int>(LOCSTR("param.power.name"), LOCSTR("param.power.desc"), _window_sz, 1, 16, 1, 2),
       new_shared<Param_int>("power", "", _window_sz, 1, 7, 1, 2),
-      new_shared<Param_int>("mode", "0 - blur, 1 - full average", _mode, 0, 1, 1, 2),
+      new_shared<Param_int>("mode", "0 - blur, 1 - full average, 2 - bidir, 3 - accurate", _mode, 0, 3, 1, 2),
     };
   }
 }; // class
