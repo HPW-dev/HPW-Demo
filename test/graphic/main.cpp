2ee7d6331b656363044affd9385db4eee2aa69be
HPW-Dev <hpwdev0@gmail.com>
2025-09-16 10:33:30 +0300

    скрипты билда, тесты и софт вырезаны и будут переделаны

--- test/graphic/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "graphic-test.hpp"
-
-int main(int argc, char *argv[]) {
-  Graphic_test app(argc, argv);
-  app.run();
-}
--- tool/editor/entity-editor-util.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Entity_editor_ctx;
-class Entity;
-
-void entity_editor_save(const Entity_editor_ctx& ctx);
-void entity_editor_load(Entity_editor_ctx& ctx);
-// настраивает флаги объекту через ImGui
-void edit_flags(Entity& entity);
