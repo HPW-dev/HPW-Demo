008674560bf7f8fca4aba936eb1790bf39d66e2d
HPW-Dev <hpwdev0@gmail.com>
2025-09-18 00:13:14 +0300

    добавлена физика шара

--- src/game/util/glass-ball.cpp
+++ src/game/util/glass-ball.cpp
@@ -3,24 +3,52 @@
 #include "graphic/image/image.hpp"
 #include "graphic/util/util-templ.hpp"
 #include "game/core/sprites.hpp"
+#include "game/core/canvas.hpp"
 #include "util/hpw-util.hpp"
 
-struct Glass_ball::Impl {
+class Glass_ball::Impl {
   Shared<Sprite> _spr {};
   Vec _pos {};
   Vec _vel {};
   constx real START_SPEED {0.9};  // с какой скоростью пулять шар
   constx real AIR_FORCE {0.9};    // сопротивление воздуха
-  constx real MASS {1};           // масса шарика
+  constx real G {9};            // ускорение падения
+  constx real MASS {10};          // массша шара
   constx real BOUND_FORCE {0.5};  // замедление шара при столкновении с краями экрана
 
+  inline void _process_bounds() {
+    cauto W = graphic::width - _spr->X();
+    cauto H = graphic::height - _spr->Y();
+    if (_pos.x < 0) {
+      _pos.x *= -1;
+      _vel.x = -_vel.x * BOUND_FORCE;
+    }
+    if (_pos.x >= W) {
+      _pos.x = W - (_pos.x - W);
+      _vel.x = -_vel.x * BOUND_FORCE;
+    }
+    if (_pos.y < 0) {
+      _pos.y *= -1;
+      _vel.y = -_vel.y * BOUND_FORCE;
+    }
+    if (_pos.y >= H) {
+      _pos.y *= H - (_pos.y - H);
+      _vel.y = -_vel.y * BOUND_FORCE;
+    }
+  }
+
+public:
   inline Impl() {
     _spr = hpw::sprites.find("resource/image/other/red glass ball.png");
     assert(_spr);
   }
 
   inline void update(Delta_time dt) {
-    
+    Vec accel(0, MASS * G);
+    accel += _vel * AIR_FORCE * -1;
+    _vel += (accel / MASS) * dt;
+    _pos += _vel * dt;
+    _process_bounds();
   }
 
   inline void draw(Image& dst) const {
