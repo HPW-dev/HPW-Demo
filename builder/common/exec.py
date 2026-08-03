debc55d63235ad60169641732b18df6bd64504b9
HPW-Dev <hpwdev0@gmail.com>
2026-08-04 01:48:41 +0500

    Запускалка процессов

---
+++ builder/common/exec.py
@@ -0,0 +1,26 @@
+
+
+import os
+import time
+import subprocess
+
+def exec_cmd(cmd, without_print=False, with_normpath=True):
+  '''
+  Запускает команды
+
+  Returns:
+    вывод команды stdout и stderr
+  '''
+  cmd_tm_st = time.time()
+  if with_normpath:
+    cmd = os.path.normpath(cmd) 
+  if without_print:
+    result = subprocess.run(cmd.split(), check=True, universal_newlines=True,
+      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  else:
+    print(cmd)
+    result = subprocess.run(cmd.split(), check=True)
+  cmd_tm_ed = time.time()
+  print(f'e.t: { round(cmd_tm_ed - cmd_tm_st, 1) }s')
+  print()
+  return result.stdout, result.stderr
