67ff60e210a09403be86a6c4fe075915667bf751
HPW-Dev <hpwdev0@gmail.com>
2026-08-23 12:45:55 +0500

    Можно находить зависимости по хедерам

--- builder/actions/compile/multi_incremental.py
+++ builder/actions/compile/multi_incremental.py
@@ -1,4 +1,3 @@
-from utils.ui import *
 import utils.fs as fs
 import re
 
@@ -43,7 +42,7 @@ def find_headers(cxx_file: str, visited=None, lines=200):
         
   return headers
 
-def make_header_map(search_dir: str):
+def make_header_map(cxx_files: list[str]):
   """
   Строит карту связей между С++ файлами
   
@@ -53,19 +52,9 @@ def make_header_map(search_dir: str):
     ('root.c'      , {'headers': ['root.h', 'subdir/a.hpp', 'subdir/b.hpp']})
   }
   """
-
-  cxx_files = []
-  cxx_files.extend(fs.find(f'{search_dir}**/*.cxx'))
-  cxx_files.extend(fs.find(f'{search_dir}**/*.cpp'))
-  cxx_files.extend(fs.find(f'{search_dir}**/*.cc'))
-  cxx_files.extend(fs.find(f'{search_dir}**/*.c'))
-
   if cxx_files == []:
-    raise FileNotFoundError('Не удалось найти зависимости между .cpp/.hpp файлами ' \
-      f'в папке "{search_dir}"')
-  
+    raise ValueError('Пустой список файлов')
   map = {}
   for cxx in cxx_files:
     map[cxx] = { 'headers': find_headers(cxx) }
-
   return map
--- builder/test-header-map.py
+++ builder/test-header-map.py
@@ -1,11 +1,17 @@
 from actions.compile.multi_incremental import make_header_map
 from utils.ui import *
+import utils.fs as fs
 
 
 print("=== Тест поиска зависимостей в .cpp/.hpp ===")
 
 search_dir = "builder/test-progs/includes/"
-map = make_header_map(search_dir)
+cxx_files = []
+cxx_files.extend(fs.find(f'{search_dir}**/*.cxx'))
+cxx_files.extend(fs.find(f'{search_dir}**/*.cpp'))
+cxx_files.extend(fs.find(f'{search_dir}**/*.cc'))
+cxx_files.extend(fs.find(f'{search_dir}**/*.c'))
+map = make_header_map(cxx_files)
 print(to_yellow('карта зависимостей:'))
 for entry in map.items():
   print(to_yellow(entry))
