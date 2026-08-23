9e64a8eead7e8cbe9c536b1ec825e7bbbf4ef4a7
HPW-Dev <hpwdev0@gmail.com>
2026-08-23 12:59:36 +0500

    + тест

--- builder/test-header-map.py
+++ builder/test-header-map.py
@@ -12,9 +12,18 @@ cxx_files.extend(fs.find(f'{search_dir}**/*.cpp'))
 cxx_files.extend(fs.find(f'{search_dir}**/*.cc'))
 cxx_files.extend(fs.find(f'{search_dir}**/*.c'))
 map = make_header_map(cxx_files)
+
 print(to_yellow('карта зависимостей:'))
 for entry in map.items():
   print(to_yellow(entry))
+
+# проверки что нужные файлы найдены:
+assert(fs.path_abs('builder/test-progs/includes/subdir/a.cxx') in map)
+assert(fs.path_abs('builder/test-progs/includes/root.c') in map)
+assert(fs.path_abs('builder/test-progs/includes/root.h') in \
+  map[fs.path_abs('builder/test-progs/includes/root.c')]['headers'])
+assert(fs.path_abs('builder/test-progs/includes/subdir/b.hpp') in \
+  map[fs.path_abs('builder/test-progs/includes/subdir/a.cxx')]['headers'])
 
 print(60*'=')
 print(to_green('> тесты пройдены'))
