9b4fb14e3a93381e5727c042fdcd458a0bdd4ff0
HPW-Dev <hpwdev0@gmail.com>
2026-08-24 12:19:29 +0500

    Не забывать сносить временную папку перед тестом

--- builder/test-incremental.py
+++ builder/test-incremental.py
@@ -20,8 +20,12 @@ ctx.with_licenses = False
 ctx.with_build_info_file = False
 prepare_build(ctx)
 
+
+# снести то, что уже собрано в прошлый раз
 test_dir = f'{ctx.tmp_dir}multi_incremental/'
 ctx.bin_dir = f'{test_dir}bin/'
+fs.rem_dir(test_dir)
+
 fs.make_dir(test_dir)
 fs.make_dir(ctx.bin_dir)
 fs.copy('builder/test-progs/single 2', test_dir)
