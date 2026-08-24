daa3a5f5525eb31bd513d3e7d2fa5894393a3255
HPW-Dev <hpwdev0@gmail.com>
2026-08-24 11:53:50 +0500

    copy теперь копирует и папки

--- builder/utils/fs.py
+++ builder/utils/fs.py
@@ -56,7 +56,10 @@ def exists(path: str):
 
 def copy(src_path: str, dst_path: str):
   '''копирование файла. Разрешена подмена на другой'''
-  shutil.copy(Path(src_path), Path(dst_path))
+  if is_dir(src_path):
+    shutil.copytree(Path(src_path), Path(dst_path))
+  else:
+    shutil.copy(Path(src_path), Path(dst_path))
 
 def equal(src_path: str, dst_path: str) -> bool:
   '''проверить что два файла одинаковы по содержанию'''
