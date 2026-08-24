8c18cc17a017e4115b4c4dee01fc057d79ca1145
HPW-Dev <hpwdev0@gmail.com>
2026-08-24 14:56:28 +0500

    Готов инкриментальный сборщик

--- builder/actions/compile/multi_incremental.py
+++ builder/actions/compile/multi_incremental.py
@@ -7,6 +7,7 @@ from utils.ui import *
 from utils.hash import blake2b
 import utils.fs as fs
 import json
+import copy
 import re
 
 def find_headers(cxx_file: str, visited=None, lines=200):
@@ -19,7 +20,6 @@ def find_headers(cxx_file: str, visited=None, lines=200):
   if visited is None:
     visited = set()
         
-  # Предотвращаем бесконечную рекурсию при циклических инклудах
   if cxx_file in visited or not fs.exists(cxx_file):
     return []
     
@@ -72,15 +72,13 @@ def make_db(header_map: dict):
   db = header_map
 
   for cxx_path, content in db.items():
-    content['c++ hash'] = blake2b(cxx_path)
+    content['hash'] = blake2b(cxx_path)
 
-    new_headers = []
+    new_headers = {}
     for header in content['headers']:
-      header_content = {
-        'path': header,
+      new_headers[header] = {
         'hash': blake2b(header),
       }
-      new_headers.append(header_content)
     content['headers'] = new_headers
 
   return db
@@ -89,12 +87,41 @@ def check_diffs(db_path: str, header_map: dict, ctx: Context) -> Rebuild_info:
   '''Ищет изменения в файлах кода сравнивая предыдущую базу сборки'''
   info = Rebuild_info()
   local_db = make_db(header_map)
-
+  
   print(to_gray(f'> загрузка {db_path}...'))
-  loaded_db = {}
   with open(db_path, "r", encoding="utf-8") as f:
     loaded_db = json.load(f)
 
+  for cxx_path, local_content in local_db.items():
+    if cxx_path in loaded_db:
+      loaded_content = loaded_db[cxx_path]
+      
+      if loaded_content['hash'] != local_content['hash']:
+        print(to_yellow(f'обнаружено изменение в файле "{cxx_path}"'))
+        info.modified_files.append(cxx_path)
+        info.rebuild_needed = True
+        continue
+        
+      for lchhp, lchpp_cnt in local_content['headers'].items():
+        if lchhp not in loaded_content['headers'] or lchpp_cnt['hash'] != loaded_content['headers'][lchhp]['hash']:
+          print(to_green(f'в файле "{cxx_path}" обнаружены изменения in "{lchhp}"'))
+          info.modified_files.append(cxx_path)
+          info.rebuild_needed = True  
+          break 
+
+    else:
+      print(to_green(f'обнаружен новый файл "{cxx_path}"'))
+      info.new_files.append(cxx_path)
+      info.rebuild_needed = True
+
+  for old_cxx in list(loaded_db.keys()):
+    if old_cxx not in local_db or not fs.exists(old_cxx):
+      print(to_red(f'обнаружено удаление файла "{old_cxx}"'))
+      info.deleted_files.append(old_cxx)
+      info.rebuild_needed = True
+  info.new_files = [path for path in info.new_files if fs.exists(path)]
+  info.modified_files = [path for path in info.modified_files if fs.exists(path)]
+  
   if info.rebuild_needed:
     print(to_green(f'> Обновление базы изменений в файлах {db_path}...'))
     with open(db_path, "w", encoding="utf-8") as f:
@@ -111,14 +138,11 @@ def check_for_rebuild(target_name: str, header_map: dict, ctx: Context) -> Rebui
   :return: Инфа - кого пересобирать
   '''
   
-  # ищем базу для пересборки
-  db_path = f'{ctx.tmp_dir}target_name.json'
+  db_path = f'{ctx.tmp_dir}{target_name}.json'
 
-  # если нашли, то сравниваем различия
   if fs.exists(db_path):
     return check_diffs(db_path, header_map, ctx)
     
-  # если не нашли, создаём новую базу и врубаем принудительную пересборку
   else:
     print(f'База для пересборки {to_yellow(target_name)} не найдена')
 
@@ -135,13 +159,19 @@ def check_for_rebuild(target_name: str, header_map: dict, ctx: Context) -> Rebui
 
 def compile_multi_incremental(tgt_src: Target, ctx: Context, host: Host) -> Rebuild_info:
   ''' Компилирует в многопотоке только изменённые файлы кода '''
-  rebuild = check_for_rebuild(tgt_src.name, make_header_map(tgt_src.sources), ctx)
+  abs_existing_sources = [fs.path_abs(src) for src in tgt_src.sources if fs.exists(src)]
+  
+  if not abs_existing_sources:
+    header_map = {}
+  else:
+    header_map = make_header_map(abs_existing_sources)
+    
+  rebuild = check_for_rebuild(tgt_src.name, header_map, ctx)
 
-  if rebuild.rebuild_needed:
-    tgt = tgt_src
-    tgt.sources.clear()
-    tgt.sources.extend(rebuild.modified_files)
-    tgt.sources.extend(rebuild.new_files)
+  files_to_build = rebuild.modified_files + rebuild.new_files
+  if rebuild.rebuild_needed and files_to_build:
+    tgt = copy.deepcopy(tgt_src)
+    tgt.sources = files_to_build
     compile_multi(tgt, ctx, host)
 
   else:
--- builder/test-incremental.py
+++ builder/test-incremental.py
@@ -25,7 +25,7 @@ test_dir = f'{ctx.tmp_dir}multi_incremental/'
 ctx.bin_dir = f'{test_dir}bin/'
 fs.rem_dir(test_dir)
 fs.rem_dir(ctx.obj_dir)
-fs.rem(f'{ctx.tmp_dir}target_name.json')
+fs.rem(f'{ctx.tmp_dir}{tgt.name}.json')
 
 fs.make_dir(test_dir)
 fs.make_dir(ctx.bin_dir)
@@ -39,15 +39,109 @@ tgt.options.extend(['Wall', 'std=c++26', 'pipe', 'O0', 'g0'])
 print(to_gray('> сборка...'))
 result = compile_multi_incremental(tgt, ctx, host)
 
-print(to_gray('> проверка...'))
+print(to_white('\n=== тест певой сборки ==='))
 assert(result.rebuild_needed)
 assert(result.new_files != [])
 assert(result.modified_files == [])
 assert(result.deleted_files == [])
 assert exists(exe_path), f"файл '{exe_path}' должен существовать"
 out, _, _ = exec_cmd([exe_path])
+print(f'*результат программы: {to_green(out)}')
+assert(out == '97997')
+
+print(to_white('\n=== тест что ничего не поменялось #1 ==='))
+result = compile_multi_incremental(tgt, ctx, host)
+assert(not result.rebuild_needed)
+assert(result.new_files == [])
+assert(result.modified_files == [])
+assert(result.deleted_files == [])
+assert exists(exe_path), f"файл '{exe_path}' должен существовать"
+out, _, _ = exec_cmd([exe_path])
 print(f'результат программы: {to_green(out)}')
 assert(out == '97997')
+
+print(to_white('\n=== тест изменения .cpp файла ==='))
+with open(f'{test_dir}subdir/func.cpp', 'w', encoding='utf-8') as f:
+  f.write(
+R'''#include "func.hpp"
+int func() { return 777; }'''
+  )
+result = compile_multi_incremental(tgt, ctx, host)
+assert(result.rebuild_needed)
+assert(result.new_files == [])
+assert(result.modified_files != [])
+assert(result.deleted_files == [])
+out, _, _ = exec_cmd([exe_path])
+print(f'результат программы: {to_green(out)}')
+assert(out == '777')
+
+print(to_white('\n=== тест изменения .hpp файла #1 ==='))
+with open(f'{test_dir}subdir/new_header.hpp', 'w', encoding='utf-8') as f:
+  f.write(
+R'''#pragma once
+constexpr static const int sixseven = 67; // думайте'''
+  )
+with open(f'{test_dir}subdir/func.hpp', 'w', encoding='utf-8') as f:
+  f.write(
+R'''#pragma once
+#include "new_header.hpp"
+int func();
+inline int func_2(int x=sixseven) { return x; }'''
+  )
+with open(f'{test_dir}main.cpp', 'w', encoding='utf-8') as f:
+  f.write(R'''#include <cstdio>
+#include "subdir/func.hpp"
+int main() { std::printf("%i", func_2()); }'''
+  )
+result = compile_multi_incremental(tgt, ctx, host)
+assert(result.rebuild_needed)
+assert(result.new_files == [])
+assert(result.modified_files != [])
+assert(result.deleted_files == [])
+out, _, _ = exec_cmd([exe_path])
+print(f'результат программы: {to_green(out)}')
+assert(out == '67')
+
+print(to_white('\n=== тест изменения .hpp файла #2 ==='))
+with open(f'{test_dir}subdir/new_header.hpp', 'w', encoding='utf-8') as f:
+  f.write(
+R'''#pragma once
+constexpr static const int sixseven = 27000;'''
+  )
+result = compile_multi_incremental(tgt, ctx, host)
+assert(result.rebuild_needed)
+assert(result.new_files == [])
+assert(result.modified_files != [])
+assert(result.deleted_files == [])
+out, _, _ = exec_cmd([exe_path])
+print(f'результат программы: {to_green(out)}')
+assert(out == '27000')
+
+print(to_white('\n=== тест удаления файлов ==='))
+fs.rem_dir(f'{test_dir}subdir/')
+with open(f'{test_dir}main.cpp', 'w', encoding='utf-8') as f:
+  f.write(R'''#include <cstdio>
+int main() { std::printf("666"); }'''
+  )
+result = compile_multi_incremental(tgt, ctx, host)
+assert(result.rebuild_needed)
+assert(result.new_files == [])
+assert(result.modified_files != [])
+assert(result.deleted_files != [])
+out, _, _ = exec_cmd([exe_path])
+print(f'результат программы: {to_green(out)}')
+assert(out == '666')
+
+print(to_white('\n=== тест что ничего не поменялось #2 ==='))
+result = compile_multi_incremental(tgt, ctx, host)
+assert(not result.rebuild_needed)
+assert(result.new_files == [])
+assert(result.modified_files == [])
+assert(result.deleted_files == [])
+assert exists(exe_path), f"файл '{exe_path}' должен существовать"
+out, _, _ = exec_cmd([exe_path])
+print(f'результат программы: {to_green(out)}')
+assert(out == '666')
 
 print('='*79)
 print(to_green('тест пройден'))
